026cd371cd092299a579fe3c39ee3c21f012488d
nely de faria <nelly.nf54@gmail.com>
2022-04-11 20:55:45 -0300

    Criando arquivo fila.h

---
+++ estrutura-dados-cpp/fila.h
@@ -0,0 +1,18 @@
+typedef int TipoItem;
+const int max_itens = 100;
+
+class Fila
+{
+  private:
+    int primeiro, ultimo;
+    TipoItem *estrutura;
+  
+  public:
+    Fila();
+    ~Fila();
+    bool estaVazio();
+    bool estaCheio();
+    void inserir(TipoItem item);
+    TipoItem remover();
+    void imprimir();
+};
