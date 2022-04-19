6d1126f6e09156ad2b6715165d94074194faf164
nely de faria <nelly.nf54@gmail.com>
2022-04-19 18:19:52 -0300

    Criando arquivo main_fila.cpp

---
+++ estrutura-dados-cpp/main_fila.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "fila.h"
+
+using namespace std;
+
+int main()
+{
+	Fila fila1;
+	int opcao;
+	TipoItem item;
+	
+	cout << "Programa gerador de filas: \n";
+	
+	do {
+		
+		cout << "Digite 0 para sair do programa!\n";
+		cout << "Digite 1 para inserir um elemento!\n";
+		cout << "Digite 2 para remover um elemento!\n";
+		cout << "Digite 3 para imprimir a fila!\n";
+		cin >> opcao;
+		
+		if (opcao == 1) {
+			cout << "Digite o elemento a ser inserido na fila: ";
+			cin >> item;
+			fila1.inserir(item);
+		} else if (opcao == 2) {
+			item = fila1.remover();
+			cout << "O elemento removido é: " << item << endl;
+		} else if (opcao == 3) {
+			fila1.imprimir();
+		}
+		
+	} while (opcao != 0);
+	
+	return 0;
+}
